fd8681f66f48a92fbe47bbfefd9e31888a1de9c3
applejxd <break.r.evo@gmail.com>
2020-01-17 17:00:39 +0900

    added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npm init -y
+npm install --save-dev \
+    textlint \
+    textlint-rule-preset-ja-spacing \
+    textlint-rule-preset-ja-technical-writing \
+    textlint-rule-spellcheck-tech-word \
+    textlint-plugin-latex2e
